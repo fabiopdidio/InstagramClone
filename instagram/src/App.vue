fceed896227c2ae99960f52652ea00c024c794bb
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-08-08 10:40:49 -0300

    add css

--- instagram/src/App.vue
+++ instagram/src/App.vue
@@ -4,4 +4,57 @@
 <script>
 export default {}
 </script>
-<style></style>
+<style scoped>
+.formulario {
+  margin: 40px auto;
+  width: 40%;
+  border-radius: 4px;
+  border: 1px solid #383737;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+  padding: 10px;
+}
+
+.input-area {
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+}
+
+.input-area input {
+  width: 100%;
+}
+
+.texto-erro {
+  color: red;
+  margin: 4px;
+}
+
+input {
+  height: 54px;
+  width: 80%;
+  border-radius: 8px;
+  border: 1px solid #756767;
+  outline: none;
+}
+
+button {
+  width: 80%;
+  height: 54px;
+  background-color: #3578e5;
+  color: white;
+  font-size: 18px;
+  border-radius: 8px;
+  border: none;
+}
+
+button:hover {
+  background-color: #286ee0;
+}
+
+.input-error {
+  border-color: red;
+}
+</style>
