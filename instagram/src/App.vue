b2f8d46350147743aa44ccfe8045bdb4051122a5
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-08-08 11:42:46 -0300

    Update App.vue

--- instagram/src/App.vue
+++ instagram/src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <form @submit="handleLogin" class="form-login">
+  <form @submit.prevent="handleLogin" class="form-login">
     <input type="text" placeholder="Digite seu email" v-model="email" />
     <input type="password" placeholder="Digite sua senha" v-model="password" />
+    <button type="submit">Login</button>
   </form>
 </template>
 
@@ -14,7 +15,7 @@ export default {
     }
   },
   methods: {
-    
+    handleLogin() {}
   }
 }
 </script>
@@ -33,7 +34,7 @@ export default {
 }
 
 .input-area {
-  width: 100%;
+  width: 80%;
   display: flex;
   flex-direction: column;
 }
@@ -53,5 +54,23 @@ input {
   border-radius: 8px;
   border: 1px solid #756767;
   outline: none;
+}
+
+button {
+  width: 80%;
+  height: 54px;
+  background-color: #3578e5;
+  color: white;
+  font-size: 18px;
+  border-radius: 8px;
+  border: none;
+}
+
+button:hover {
+  background-color: #286ee0;
+}
+
+.input-error {
+  border-color: red;
 }
 </style>
