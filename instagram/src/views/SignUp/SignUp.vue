611e9779988674354aa0df9871da15506cff6c14
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-08-10 11:34:03 -0300

    update

--- instagram/src/views/SignUp/SignUp.vue
+++ instagram/src/views/SignUp/SignUp.vue
@@ -1,52 +1,78 @@
 <template>
-  <form class="form-login">
+  <form @submit.prevent="handleCreateAccount" class="form-login">
     <h2>Criar conta</h2>
 
-    <label for="name"> Nome completo </label>
-    <input id="name" v-model="name" />
+    <div class="form-element">
+      <label for="name"> Nome completo </label>
+      <input class="form-input" id="name" v-model="name" />
+    </div>
 
-    <label for="email"> Email </label>
-    <input id="email" v-model="email" />
+    <div class="form-element">
+      <label for="email"> Email </label>
+      <input class="form-input" id="email" v-model="email" />
+    </div>
 
-    <label for="phone"> Telefone </label>
-    <input id="phone" v-model="phone" />
+    <div class="form-element">
+      <label for="phone"> Telefone </label>
+      <input class="form-input" id="phone" v-model="phone" />
+    </div>
 
-    <label for="password"> Senha </label>
-    <input id="password" type="password" v-model="password" />
+    <div class="form-element">
+      <label for="password"> Senha </label>
+      <input class="form-input" id="password" type="password" v-model="password" />
+    </div>
 
-    <label for="verifyPassword"> Confirma senha </label>
-    <input id="verifyPassword" type="password" v-model="verifyPassword" />
+    <div class="form-element">
+      <label for="verifyPassword"> Confirma senha </label>
+      <input class="form-input" id="verifyPassword" type="password" v-model="verifyPassword" />
+    </div>
 
-    {{ sponsor }}
+    <div class="form-element">
+      <label for="sponsor">Patrocinador</label>
+      <select id="sponsor" v-model="sponsor" class="form-select">
+        <option value="">Sem patrocinador</option>
+        <option value="Coca cola">Coca cola</option>
+        <option value="Lab 365">Lab 365</option>
+        <option value="Zucchetti">Zucchetti</option>
+      </select>
+    </div>
 
-    <label for="sponsor">Patrocinador</label>
-    <select id="sponsor" v-model="sponsor">
-      <option value="">Sem patrocinador</option>
-      <option value="Coca cola">Coca cola</option>
-      <option value="Lab 365">Lab 365</option>
-      <option value="Zucchetti">Zucchetti</option>
-    </select>
+    <div class="form-element">
+      <label for="bio">Sua biografia</label>
+      <textarea id="bio" v-model="bio" class="form-textarea"> </textarea>
+    </div>
 
-    <label for="bio">Sua biografia</label>
-    <textarea id="bio" v-model="bio"> </textarea>
+    <div class="form-element">
+      <p>Selecione um tipo de plano:</p>
 
-    {{ confirmTerms ? 'aceite' : 'nao aceite' }}
+      <div class="form-radio">
+        <input id="bronze" type="radio" value="1" v-model="planType" />
+        <label for="bronze">Bronze</label>
+      </div>
 
-    <label id="confirmTerms">
-      <input type="checkbox" id="confirmTerms" v-model="confirmTerms" /> Aceita termos de Uso
-    </label>
+      <div class="form-radio">
+        <input id="prata" type="radio" value="2" v-model="planType" />
+        <label for="prata">Prata</label>
+      </div>
 
-    <input id="bronze" type="radio" value="1" v-model="planType" />
-    <label for="bronze">Bronze</label>
+      <div class="form-radio">
+        <input id="premium" type="radio" value="3" v-model="planType" />
+        <label for="premium">Premium</label>
+      </div>
+    </div>
 
-    <input id="prata" type="radio" value="2" v-model="planType" /> <label for="prata">Prata</label>
+    <div class="form-element">
+      <label id="confirmTerms">
+        <input type="checkbox" id="confirmTerms" v-model="confirmTerms" /> Aceita termos de Uso
+      </label>
+    </div>
 
-    <input id="premium" type="radio" value="3" v-model="planType" />
-    <label for="premium">Premium</label>
+    <button type="submit">Criar conta</button>
   </form>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
@@ -60,17 +86,16 @@ export default {
       confirmTerms: true,
       planType: '2'
     }
+  },
+  methods: {
+    handleCreateAccount() {
+      // SCHEMA VALIDATION
+    }
   }
 }
 </script>
 
 <style scoped>
-.error-box {
-  background: tomato;
-  width: 80%;
-  color: #fff;
-}
-
 .form-login {
   margin: 40px auto;
   width: 40%;
@@ -80,14 +105,46 @@ export default {
 
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 10px;
   align-items: center;
 
   padding: 10px;
 }
 
-.input-area {
+.form-element {
+  display: flex;
+  flex-direction: column;
   width: 80%;
+}
+
+.form-select,
+.form-input,
+.form-textarea {
+  height: 34px;
+  width: 100%;
+  border-radius: 8px;
+  border: 1px solid #756767;
+  outline: none;
+}
+
+.form-textarea {
+  height: 68px;
+}
+
+.form-radio {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+}
+
+.error-box {
+  background: tomato;
+  width: 80%;
+  color: #fff;
+}
+
+.input-area {
+  width: 100%;
 
   display: flex;
   flex-direction: column;
@@ -100,14 +157,6 @@ export default {
 .texto-erro {
   color: red;
   margin: 4px;
-}
-
-input {
-  height: 54px;
-  width: 80%;
-  border-radius: 8px;
-  border: 1px solid #756767;
-  outline: none;
 }
 
 button {
