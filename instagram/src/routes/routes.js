7687dc8512c7136ebd1b2c078b6b85c625ac3c9e
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-08-13 19:59:51 -0300

    Update routes.js

--- instagram/src/routes/routes.js
+++ instagram/src/routes/routes.js
@@ -5,7 +5,7 @@ import Home from '../views/Home/Home.vue'
 import Login from '../views/Login/Login.vue'
 import SignUp from '../views/SignUp/SignUp.vue'
 
-// Criar roteamento
+// Criar roteamento, define URL
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
